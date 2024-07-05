0355a7860f4ab21d1678570b2c995b4564103046
Jan Francesca Logroño <janfrancesca.logrono.20@usjr.edu.ph>
2024-07-06 00:17:19 +0800

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
 
-
-    Route::get('/', function () {
-        return view('welcome');
-    });
-    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-    Route::post('/register', [AuthController::class, 'register']);
-
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
-
-    Route::get('/logout', [AuthController::class, 'logout']);
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
     Route::middleware('auth')->group(function () {
         Route::view('/home', 'homepage');
     });
